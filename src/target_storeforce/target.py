2d4306fefbbde03ef8089bd36e32abcbac8233d2
Adam Strickland <adam.strickland@gmail.com>
2021-06-16 09:34:27 -0400

    adds basic tests

--- src/target_storeforce/target.py
+++ src/target_storeforce/target.py
@@ -1,20 +1,26 @@
 import io
 import sys
 import tempfile
+import json
 
 import singer
 from singer import utils
 
-from bonobos_singer_support.sftp import sftp_connection
+from bonobos_singer_support import sftp
 from .generator import generate_csv_from_messages
+
 
 LOGGER = singer.get_logger()
 
 ENCODING = "utf-8"
 
+DEFAULT_DESTINATION_FOLDER = "/"
+
+
 class Target:
     def __init__(self, config):
         self.config = config
+        self.connection = sftp.sftp_connection(config=self.config)
 
     def run(self, stream_in, stream_out):
         buffered_stream = stream_in.buffer
@@ -22,62 +28,32 @@ class Target:
         message_buffer = io.TextIOWrapper(buffered_stream, encoding=ENCODING)
 
         with tempfile.TemporaryDirectory() as temp_dir:
-            (generated_file_path, record_count, state) = generate_csv_from_messages(message_buffer, self.config, temp_dir)
+            (generated_file_path,
+             _record_count,
+             state,
+             _filename) = generate_csv_from_messages(message_buffer,
+                                                     self.config,
+                                                     temp_dir)
 
             self.egress_file(generated_file_path)
 
             self.output_state_to_stream([state], stream_out)
 
-    def egress_file(self, path_to_file):
-        connection = sftp_connection(host=self.config.get("host"),
-                                     port=self.config.get("port"),
-                                     username=self.config.get("username"),
-                                     password=self.config.get("password"),)
-        connection.put_file(path_to_file)
+    def egress_file(self, path_to_data_file, destination_file_path=None):
+        def destfile(infile):
+            return "foobar"
+
+        destination_folder_path = self.config.get("destination_path", DEFAULT_DESTINATION_FOLDER)
+
+        destination_file_name = destfile(path_to_data_file)
+
+        self.connection.put_file(file=path_to_data_file,
+                                 destination_path=destination_folder_path,
+                                 fname=destination_file_name,)
 
     def output_state_to_stream(self, states, stream):
         for state in states:
-            line - json.dumps(state)
+            line = json.dumps(state)
             LOGGER.info(f"Emitting state {line}")
             stream.write(f"{line}\n")
             stream.flush()
-
-
-        # for message in message_buffer:
-        #     try:
-        #         parsed_message = singer.parse_message(message).asdict()
-
-        #         if parsed_message["type"] == "RECORD"
-        #           handle_record(parsed_message, file_handle)
-        #         elif parsed_message["type"] == "SCHEMA"
-        #     except json.decode.JSONDecodeError:
-        #         raise
-
-        # message_set = self.categorized_messages(buffered_stream)
-
-        # schemas = message_set.schemas
-
-        # states = message_set.states
-
-        # records = message_set.records
-
-        # generated_file = self.file_from_records(records)
-
-        # self.egress_file(generated_file)
-
-        # self.output_state_to_stream(states, stream_out)
-
-    # def categorized_messages(self, stream):
-    #     raw_messages = io.TextIOWrapper(stream, encoding=ENCODING)
-
-    #     try:
-
-
-    # def file_from_records(self, records):
-    #     with tempfile.TemporaryDirectory() as temp_dir:
-    #         LOGGER.debug(f"Using temp directory {temp_dir}")
-
-
-    #         LOGGER.debug(f"Generated file at {generated_file_path}")
-
-    #         return generated_file_path
